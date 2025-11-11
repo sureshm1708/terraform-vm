e97514a58cb2151467dd1fa1c3951db2618bca94
sureshm1708 <sureshm1708@zohomail.in>
2025-11-11 14:53:18 +0530

    updating original files

--- myproviders.tf
+++ myproviders.tf
@@ -1,6 +1,3 @@
 provider "aws" {
-    access_key = "AKIA6BXYKOZOC37VKEE2"
-    secret_key = ""
     version = "6.20.0"
-    region = "us-east-1"
 }
--- myresources.tf
+++ myresources.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "test_instance" {
-ami = "ami-047248cf574e28ecc"
+
+ami = "ami-0b0ea68c435eb488d"
 instance_type = "t2.nano"
+
 }
+
